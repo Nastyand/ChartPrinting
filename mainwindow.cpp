3204879a45861fd7f184c99965039046aae6f90a
Anastasia <aandreeva1313@mail.ru>
2023-06-11 17:50:30 +0700

    Создание окна

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,67 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Задание минимального размера окна
+    setMinimumSize(500, 300);
+    // Задание стандартного размера окна
+    resize(800, 600);
+
+    // Создание QWidget
+    std::unique_ptr<QWidget> widget = std::make_unique<QWidget>(this);
+
+    // Создание кнопки "открыть папку"
+    openFolderButton = std::make_unique<QPushButton>("Открыть папку", this);
+    openFolderButton->setStyleSheet("border: 1px solid black; padding: 4px;");
+
+    // Создание метки "тип диаграммы"
+    chartTypeLabel = std::make_unique<QLabel>("Тип диаграммы:", this);
+    chartTypeLabel->setStyleSheet("border: 1px solid black; padding: 3px;");
+
+    // Создание списка типов диаграмм
+    chartTypesComboBox = std::make_unique<QComboBox>(this);
+    chartTypesComboBox->addItem("Столбчатая диаграмма");
+    chartTypesComboBox->addItem("Круговая диаграмма");
+    chartTypesComboBox->setStyleSheet("border: 1px solid black; padding: 3px;");
+
+    // Создание флажка для выбора черно-белой диаграммы
+    chartBWCheckBox = std::make_unique<QCheckBox>("Черно-белая диаграмма", this);
+    chartBWCheckBox->setStyleSheet("border: 1px solid black; padding: 4px;");
+
+    // Создание кнопки "печать"
+    printingButton = std::make_unique<QPushButton>("Печать", this);
+    printingButton->setStyleSheet("border: 1px solid black; padding: 4px;");
+
+    // Создание разделителя
+    QTableView *tableView1 = new QTableView;
+    QTableView *tableView2 = new QTableView;
+
+    splitter = std::make_unique<QSplitter>();
+    splitter->addWidget(tableView1);
+    splitter->addWidget(tableView2);
+
+    // Создание QHBoxLayout
+    std::unique_ptr<QHBoxLayout> hLayout = std::make_unique<QHBoxLayout>();
+    hLayout->addWidget(openFolderButton.get());
+    hLayout->addWidget(chartTypeLabel.get());
+    hLayout->addWidget(chartTypesComboBox.get());
+    hLayout->addWidget(chartBWCheckBox.get());
+    hLayout->addWidget(printingButton.get());
+
+    // Создание QVBoxLayout
+    std::unique_ptr<QVBoxLayout> vLayout = std::make_unique<QVBoxLayout>();
+    vLayout->addLayout(hLayout.release());
+    vLayout->addWidget(splitter.release());
+
+    // Установка QVBoxLayout в виджет
+    widget->setLayout(vLayout.release());
+    setCentralWidget(widget.release());
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QComboBox>
+#include <QCheckBox>
+#include <QSplitter>
+#include <QMessageBox>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLineEdit>
+#include <QTableView>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +28,12 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    std::unique_ptr<QPushButton> openFolderButton; // Кнопка "открыть папку"
+    std::unique_ptr<QLabel> chartTypeLabel;        // Метка "тип диаграммы"
+    std::unique_ptr<QComboBox> chartTypesComboBox; // Типы диаграмм
+    std::unique_ptr<QCheckBox> chartBWCheckBox;    // Выбор черно-белой диаграммы
+    std::unique_ptr<QPushButton> printingButton;   // Кнопка "печать"
+    std::unique_ptr<QSplitter> splitter;           // Разделитель
 };
 #endif // MAINWINDOW_H
